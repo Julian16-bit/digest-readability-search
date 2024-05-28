1ff6220fca7cd537d42062a80f7851baa2ef9a49
Julian16-bit <126834892+Julian16-bit@users.noreply.github.com>
2024-05-28 15:14:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,20 @@ st.set_page_config(layout="wide")
 df1 = pd.read_csv('digest_descriptives_merged.csv')
 df2 = pd.read_csv('digest_descriptives_merged_highschool_bestversionL6V2.csv')
 
-user_input = st.text_input("", placeholder="🔍 Search for a section number", label_visibility="collapsed")
+tab1, tab2 = st.tabs(["All Sections", "Search"])
 
-if user_input:
-    section = df1.loc[df1['section_number']==user_input]
-    section = section[['section_text', 'flesch_kincaid_grade', 'gunning_fog', 'sentence_length_mean']]
-    simple_section = df2.loc[df2['section_number']==user_input]
-    simple_section = simple_section[['simplified_text', 'flesch_kincaid_grade', 'gunning_fog', 'sentence_length_mean']]
-    st.table(section)
-    st.table(simple_section)
+with tab1:
+    fig = px.scatter(df1, x=df1.index, y='flesh_kincaid_grade', title="All Sections Readability", height = 1000)
+    fig.update_layout(xaxis_title='',margin_b=400, margin_l = 40, margin_r=10, margin_t=50)
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+with tab2:
+    user_input = st.text_input("", placeholder="🔍 Search for a section number", label_visibility="collapsed")
+    
+    if user_input:
+        section = df1.loc[df1['section_number']==user_input]
+        section = section[['section_text', 'flesch_kincaid_grade', 'gunning_fog', 'sentence_length_mean']]
+        simple_section = df2.loc[df2['section_number']==user_input]
+        simple_section = simple_section[['simplified_text', 'flesch_kincaid_grade', 'gunning_fog', 'sentence_length_mean']]
+        st.table(section)
+        st.table(simple_section)
